9ac715a7ffad69e211f3289dd017969f975a50a0
Athena6666 <88647638+Athena6666@users.noreply.github.com>
2024-02-03 12:01:33 +0800

    chore: update

--- myTools/src/App.vue
+++ myTools/src/App.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-
+const imgs = [
+  'Weixin Image_20231222090523.jpg',
+  'Weixin Image_20231222090756.jpg',
+  'Weixin Image_20231222090903.jpg',
+  'Weixin Image_20231222091407.jpg'
+]
 </script>
 
 <template>
-<div>
-  <el-carousel height="150px">
-    <el-carousel-item v-for="item in 4" :key="item">
-      <h3 class="small justify-center" text="2xl">{{ item }}</h3>
-    </el-carousel-item>
-  </el-carousel>
-</div>
+  <div class="flex mt-0">
+    <el-carousel class="w-1600px h-600px mt-0px">
+      <el-carousel-item v-for="item in imgs" :key="item">
+        {{ item }}
+        <el-image :src="'./assets/imgs/' + item" fit="fill"></el-image>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- myTools/src/main.ts
+++ myTools/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'virtual:uno.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
