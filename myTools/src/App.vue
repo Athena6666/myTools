0b7ca69ea70dbf850a16c46726881126b4cd4890
Athena6666 <88647638+Athena6666@users.noreply.github.com>
2024-02-03 11:03:54 +0800

    chore: update

--- myTools/src/App.vue
+++ myTools/src/App.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div>
+  <el-carousel height="150px">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+    </el-carousel-item>
+  </el-carousel>
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
